443dcf5fd9b15cdf166e6d00a0f34fd76288bfd2
Brett Roper <roper.brett@gmail.com>
2025-02-26 15:32:48 +0800

    Update sql with PKs

--- db/moto.sql
+++ db/moto.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `members` (
-  `member_id` integer,
+  `member_id` integer PRIMARY KEY,
   `username` varchar(255),
   `password` varchar(255),
   `email` varchar(255),
@@ -7,19 +7,19 @@ CREATE TABLE `members` (
 );
 
 CREATE TABLE `riders` (
-  `rider_id` integer,
+  `rider_id` integer PRIMARY KEY,
   `name` varchar(255),
   `active` boolean
 );
 
 CREATE TABLE `events` (
-  `event_id` integer,
+  `event_id` integer PRIMARY KEY,
   `name` varchar(255),
   `date` date
 );
 
 CREATE TABLE `bids` (
-  `bid_id` integer,
+  `bid_id` integer PRIMARY KEY,
   `member_id` integer,
   `rider_id` integer,
   `event_id` integer,
