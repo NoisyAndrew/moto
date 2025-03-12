23ca43fef995320805e5d2cb91c3485e0637acb6
Brett Roper <roper.brett@gmail.com>
2025-03-12 14:06:55 +0800

    Add example data

---
+++ db/example-data/example-queries.sql
@@ -0,0 +1,13 @@
+/* raw table data */
+select * from riders;
+select * from members;
+select * from events;
+select * from bids;
+
+/* join data */
+select b.bid_id, m.username, r.name, e.name, amount
+from bids b
+join members m on b.member_id = m.member_id
+join riders r on b.rider_id = r.rider_id
+join events e on b.event_id = e.event_id
+;
