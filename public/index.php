b436914b042602ea84f6e66287b5a23b7e30bbe7
Brett Roper <roper.brett@gmail.com>
2025-03-17 17:10:35 +0800

    Name magic numbers

--- public/index.php
+++ public/index.php
@@ -17,33 +17,35 @@
         <br>
 
         <?php
+        // constants
+        $SECONDS_HOUR = 3600; // 60 * 60
+        $SECONDS_DAY = 86400; // 24 * 60 * 60
+
+        // set timezone
+        date_default_timezone_set('UTC');
+
         // check it's not a race day.
         $bidsok = 0;
         $pastrace = 0;
         $roundnumber = 1;
-        date_default_timezone_set('UTC');
         $raceday = file('dates.txt');
         $today = time();
-        // $today=time()-10800;
 
         foreach ($raceday as $race) {
             $race = strtotime($race);
-            //    $race = $race + 18000;
-            if ($today < ($race + 79200) && $today > $race) {
+            if ($today < ($race + (22 * $SECONDS_HOUR)) && $today > $race) {
                 echo "<h2>Bids are closed.</H2><p>It's Race week of Round " . $roundnumber . ". <br>So cool your heals untill the next round.<p> See below for who got what rider.";
 
                 $bidsok = 1;
-                //echo $race. " -- ".$qualifying;
+                
                 include 'rider-bids.txt';
-                $hours = round(($today - ($race + 86400)) / 60 / 60);
+                $hours = round(($today - ($race + $SECONDS_DAY)) / $SECONDS_HOUR);
                 echo "<p>Next race is <h3>" . date('d-F-Y', strtotime($raceday[$roundnumber])) . "</H3><p>and biding will be open again in" . $hours . " hours time.<br>Then closing 12am UTC the day of this race.";
-                //include 'comment-box-only.php';
             }
 
             if ($today < $race && $pastrace == 0) {
                 $thisrace = $race;
                 $pastrace = 1;
-                //echo " if#2, ";
             }
             ++$roundnumber;
         }
@@ -53,21 +55,18 @@
 
             // do count down to cut off
             $diff = $thisrace - $today;
-            $next_day = intval($diff / 86400);
-            $next_hour = intval(($diff - ($next_day * 86400)) / 3600);
-            $next_minute = intval(($diff - ($next_day * 86400) - ($next_hour * 3600)) / 60);
+            $next_day = intval($diff / $SECONDS_DAY);
+            $next_hour = intval(($diff - ($next_day * $SECONDS_DAY)) / $SECONDS_HOUR);
+            $next_minute = intval(($diff - ($next_day * $SECONDS_DAY) - ($next_hour * $SECONDS_HOUR)) / 60);
             echo "<h3>The next bid cut off is " . $next_day . " days " . $next_hour . " hours " . $next_minute . " minutes</h3><br>";
 
-            // do page diplay   
+            // do page display   
             echo "<div class='messagebox'>";
             readfile('message.txt');
             echo "</div><br><br>";
-            include 'balance-table.php';
-            echo "<br>";
-            include 'comment-box-only.php';
+            // include 'balance-table.php';
             echo "<br><br>";
             readfile('login-table.html');
-
 
             echo "<br><div class='messagebox'>";
             echo "<h4>Payouts </h4> <p> Incase you have forgotten or never really knew?  The payouts run <br>30, 24, 19, 15, 12, 10, 9, 8.";
