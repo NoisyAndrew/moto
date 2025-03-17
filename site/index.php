ed9cb4092dea2bf3a5ec2260233e7e587ab19b6a
Brett Roper <brett.roper@uwa.edu.au>
2025-03-17 12:37:48 +0800

    Change login table to html file

--- site/index.php
+++ site/index.php
@@ -30,13 +30,13 @@
             $race = strtotime($race);
             //    $race = $race + 18000;
             if ($today < ($race + 79200) && $today > $race) {
-                echo "<H2>Bids are closed.</H2><p>It's Race week of Round " . $roundnumber . ". <br>So cool your heals untill the next round.<p> See below for who got what rider.";
+                echo "<h2>Bids are closed.</H2><p>It's Race week of Round " . $roundnumber . ". <br>So cool your heals untill the next round.<p> See below for who got what rider.";
 
                 $bidsok = 1;
                 //echo $race. " -- ".$qualifying;
                 include 'rider-bids.txt';
                 $hours = round(($today - ($race + 86400)) / 60 / 60);
-                echo "<p>Next race is <H3>" . date('d-F-Y', strtotime($raceday[$roundnumber])) . "</H3><p>and biding will be open again in" . $hours . " hours time.<br>Then closing 12am UTC the day of this race.";
+                echo "<p>Next race is <h3>" . date('d-F-Y', strtotime($raceday[$roundnumber])) . "</H3><p>and biding will be open again in" . $hours . " hours time.<br>Then closing 12am UTC the day of this race.";
                 //include 'comment-box-only.php';
             }
 
@@ -56,26 +56,25 @@
             $next_day = intval($diff / 86400);
             $next_hour = intval(($diff - ($next_day * 86400)) / 3600);
             $next_minute = intval(($diff - ($next_day * 86400) - ($next_hour * 3600)) / 60);
-            echo "<H3>The next bid cut off is " . $next_day . " days " . $next_hour . " hours " . $next_minute . " minutes</H3><BR>";
+            echo "<h3>The next bid cut off is " . $next_day . " days " . $next_hour . " hours " . $next_minute . " minutes</h3><br>";
 
             // do page diplay   
             echo "<div class='messagebox'>";
-            include 'message.txt';
-            echo "</div><BR><BR>";
+            readfile('message.txt');
+            echo "</div><br><br>";
             include 'balance-table.php';
-            echo "<BR>";
-
+            echo "<br>";
             include 'comment-box-only.php';
-            echo "<BR><BR>";
-            include 'login-table.php';
+            echo "<br><br>";
+            readfile('login-table.html');
 
 
-            echo "<BR><div class='messagebox'>";
-            echo "<h4>Payouts </h4> <P> Incase you have forgotten or never really knew?  The payouts run <br>30, 24, 19, 15, 12, 10, 9, 8.";
+            echo "<br><div class='messagebox'>";
+            echo "<h4>Payouts </h4> <p> Incase you have forgotten or never really knew?  The payouts run <br>30, 24, 19, 15, 12, 10, 9, 8.";
             echo "<p>Starting at first and running to eigth. ";
             echo "So bidding zero for a    shaggy rider who finishes seventh or eighth is pretty cool";
-            echo "<center><a href='http://moto.partymeeple.com.au/last-bids.php'>Click here to view the last round's bids</a></center><BR>";
-            echo "</div><BR><BR>";
+            echo "<center><a href='http://moto.partymeeple.com.au/last-bids.php'>Click here to view the last round's bids</a></center><br>";
+            echo "</div><br><br>";
         }
 
         ?>
